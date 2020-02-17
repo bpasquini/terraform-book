4d86051949551546937691ca76441905f38c5d1f
Brian Pasquini <pasquini@cmu.edu>
2020-02-17 13:34:59 -0500

    Added provider block

--- workspaces_code/main.tf
+++ workspaces_code/main.tf
@@ -9,9 +9,13 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-2"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
 }
 
 
