a7805d55d038e5de2b90d2b578c7ae7a248a3456
Brian Pasquini <pasquini@cmu.edu>
2020-02-18 16:58:43 -0500

    Added locals to module

--- webserver-cluster-project/modules/services/webserver-cluster/main.tf
+++ webserver-cluster-project/modules/services/webserver-cluster/main.tf
@@ -37,6 +37,14 @@ data "template_file" "user_data" {
     }
 }
 
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
 resource "aws_launch_configuration" "example" {
   image_id          = "ami-0c55b159cbfafe1f0"
   instance_type     = var.instance_type
@@ -77,7 +85,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
     load_balancer_arn = aws_lb.example.arn
-    port              = 80
+    port              = local.http_port
     protocol          = "HTTP"
 
     # By default, return a simple 404 page
@@ -130,8 +138,8 @@ resource "aws_security_group" "instance" {
     ingress {
         from_port   = var.server_port
         to_port     = var.server_port
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = local.tcp_protocol
+        cidr_blocks = local.all_ips
     }
 }
 
@@ -140,17 +148,17 @@ resource "aws_security_group" "alb" {
 
     # Allow inbound HTTP requests
     ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port   = local.http_port
+        to_port     = local.http_port
+        protocol    = local.tcp_protocol
+        cidr_blocks = local.all_ips
     }
 
     # Allow all outbound requests
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port   = local.any_port
+        to_port     = local.any_port
+        protocol    = local.any_protocol
+        cidr_blocks = local.all_ips
     }
 }
