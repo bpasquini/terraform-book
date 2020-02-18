f666d16b83b25d51fc6066654a7bcb79917afd1c
Brian Pasquini <pasquini@cmu.edu>
2020-02-18 14:57:43 -0500

    Finished stage folder isolation environment

--- webserver-cluster-project/stage/services/webserver-cluster/main.tf
+++ webserver-cluster-project/stage/services/webserver-cluster/main.tf
@@ -13,16 +13,8 @@ terraform {
   }
 }
 
-data "aws_vpc" "default" {
-    default = true
-}
-
-data "aws_subnet_ids" "default" {
-    vpc_id = data.aws_vpc.default.id
-}
-
 data "terraform_remote_state" "db" {
-    backend "s3"
+    backend = "s3"
 
     config = {
         bucket = "terraform-up-and-running-state-brp20200214"
@@ -31,18 +23,31 @@ data "terraform_remote_state" "db" {
     }
 }
 
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
+
+data "template_file" "user_data" {
+    template = file("user_data.sh")
+
+    vars = {
+        server_port = var.server_port
+        db_address = data.terraform_remote_state.db.outputs.address
+        db_port = data.terraform_remote_state.db.outputs.port
+    }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id          = "ami-0c55b159cbfafe1f0"
   instance_type     = "t2.micro"
   security_groups   = [aws_security_group.instance.id]
 
-  user_data =   <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-                echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+  user_data = data.template_file.user_data.rendered
+
   # Required when using a lauch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
   lifecycle {
