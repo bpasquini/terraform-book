77f587c1c5366949f1c40a821b7e4ca7540b367c
Brian Pasquini <pasquini@cmu.edu>
2020-02-18 16:02:00 -0500

    Created module for webserver-cluster

--- webserver-cluster-project/prod/services/webserver-cluster/main.tf
+++ webserver-cluster-project/prod/services/webserver-cluster/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "webserver_cluster" {
+module "webseerver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webservers-stage"
+  cluster_name = "webservers-prod"
   db_remote_state_bucket = "terraform-up-and-running-state-brp20200214"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key = "prod/datastores/mysql/terraform.tfstate"
 }
